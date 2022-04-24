e46f31d3e8840266f068507b0638b6e99d54783a
RhuanGabriel1 <74109343+RhuanGabriel1@users.noreply.github.com>
2022-04-24 17:00:07 -0300

    commit "delete de pastas não usadas"

--- src/main/java/br/com/letscode/postosaude/paciente/SexoEnum.java
+++
@@ -1,7 +0,0 @@
-package br.com.letscode.postosaude.paciente;
-
-public enum SexoEnum {
-    MASCULINO,
-    FEMININO,
-    OUTRO
-}
--- src/main/java/br/com/letscode/postosaude/profissionais/CargosEnum.java
+++
@@ -1,8 +0,0 @@
-package br.com.letscode.postosaude.profissionais;
-
-public enum CargosEnum {
-    PROFISSIONAL_ADMINISTRATIVO,
-    PROFISSIONAL_ESTOQUE,
-    PROFISSIONAL_RH,
-    PROFISSIONAL_SAUDE
-}
